160768aef3b47ade1c3aee97e17c748a8e28d08d
Mjh910101 <408951390@qq.com>
2016-08-04 17:47:40 +0800

    bug

--- app/src/main/java/com/express/subao/activitys/MainActivity.java
+++ app/src/main/java/com/express/subao/activitys/MainActivity.java
@@ -102,10 +102,10 @@ public class MainActivity extends BaseActivity {
     @ViewInject(R.id.main_tap_tapLayout)
     private LinearLayout tapLayout;
 
-    private MainFrameLayoutV2 mainFrameLayout;
+    private MainFrameLayoutV4 mainFrameLayout;
     private RebateFrameLayout rebateFrameLayout;
     private ShoppingCarFrameLayout shoppingCarFrameLayout;
-    private UserFrameLayout userFrameLayout;
+    private UserFrameLayoutV2 userFrameLayout;
     private FragmentManager fragmentManager;
 
 
@@ -318,7 +318,7 @@ public class MainActivity extends BaseActivity {
         userIcon.setImageResource(R.drawable.main_tap_on_user_icon);
         userText.setTextColor(ColorHandler.getColorForID(context, R.color.text_orange));
         if (userFrameLayout == null) {
-            userFrameLayout = new UserFrameLayout();
+            userFrameLayout = new UserFrameLayoutV2();
             transaction.add(R.id.main_content, userFrameLayout);
         } else {
             transaction.show(userFrameLayout);
@@ -359,7 +359,7 @@ public class MainActivity extends BaseActivity {
 //        scanningIcon.setVisibility(View.VISIBLE);
         titleIcon.setVisibility(View.VISIBLE);
         if (mainFrameLayout == null) {
-            mainFrameLayout = new MainFrameLayoutV2();
+            mainFrameLayout = new MainFrameLayoutV4();
             transaction.add(R.id.main_content, mainFrameLayout);
         } else {
             transaction.show(mainFrameLayout);
--- app/src/main/java/com/express/subao/views/FloatListView.java
+++ app/src/main/java/com/express/subao/views/FloatListView.java
@@ -64,23 +64,45 @@ public class FloatListView extends ListView {
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
+
+        Log.e("distance", "******************** 1 *********************");
+
         if (ev.getAction() == MotionEvent.ACTION_DOWN) {
             lastY = (int) ev.getRawY();
 //            canScrollDistance = ((ViewGroup) getParent()).getChildAt(0).getHeight();
+
             Log.e("distance", "Distance : " + canScrollDistance);
+
+            return super.onTouchEvent(ev);
+        }
+        if (ev.getAction() == MotionEvent.ACTION_UP) {
+            View view = scrollParent;
+            if (view.getScrollY() <= 0) {
+                lastY = 0;
+            } else {
+                lastY = (int) ev.getRawY();
+            }
             return super.onTouchEvent(ev);
         }
         int currentY = (int) ev.getRawY();
+
         Log.e("distance", "currentY : " + currentY + " , lastY : " + lastY);
+
         if (currentY < lastY) {
+
             Log.e("distance", "******************** currentY < lastY *********************");
+
 //            View view = ((ViewGroup) getParent());
             View view = scrollParent;
             if (view.getScrollY() < canScrollDistance) {
+
                 Log.e("distance", "scrollBy : " + (lastY - currentY));
+
                 view.scrollBy(0, (int) (lastY - currentY));
                 if (view.getScrollY() > canScrollDistance) {
+
                     Log.e("distance", "scrollTo : " + canScrollDistance);
+
                     view.scrollTo(0, canScrollDistance);
                 }
                 lastY = currentY;
@@ -88,19 +110,27 @@ public class FloatListView extends ListView {
             }
 
         } else if (getFirstVisiblePosition() == 0) {
+
             Log.e("distance", "******************** currentY > lastY *********************");
+
             View child = getChildAt(0);
             if (child != null) {
                 if (child.getTop() == 0) {
                     //            View view = ((ViewGroup) getParent());
                     View view = scrollParent;
+
                     Log.e("distance", "ScrollY : " + view.getScrollY());
+
                     if (view.getScrollY() > 0) {
+
                         Log.e("distance", "scrollBy : " + (lastY - currentY));
+
                         view.scrollBy(0, (int) (lastY - currentY));
                         lastY = currentY;
                         if (view.getScrollY() < 0) {
+
                             Log.e("distance", "scrollTo : " + 0);
+
                             view.scrollTo(0, 0);
                         }
                         return true;
